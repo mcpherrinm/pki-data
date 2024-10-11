127615c6d98c44228da9d08e2707a924c011b123
Matthew McPherrin <git@mcpherrin.ca>
2024-10-11 16:00:50 +0100

    make the ct logs list fancier

--- js/ct.js
+++ js/ct.js
@@ -1,22 +1,152 @@
+function dropPrefix(prefix, str) {
+    if (str.startsWith(prefix)) {
+        str = str.substring(prefix.length)
+    }
+    return str.trim()
+}
+
+function dropSuffix(suffix, str) {
+    if (str.endsWith(suffix)) {
+        str = str.substring(0, str.length - suffix.length)
+    }
+    return str.trim()
+}
+
+function normalizeDescription(operator, description) {
+    let orig = description
+    description = dropPrefix(operator, description)
+    description = dropSuffix("CT Log", description)
+    description = dropSuffix("CT log", description)
+    description = dropSuffix("Log", description)
+    description = dropSuffix("log", description)
+    description = dropPrefix("'", description)
+    description = dropSuffix("'", description)
+
+    // "StartCom log" etc goes to 0, so just return the original
+    if (description.length === 0) {
+        return orig
+    }
+
+    return description
+}
+
+function normalizeState(state) {
+    let entries = Object.entries(state);
+    if (entries.length !== 1) {
+        console.log("Unexpected state length", entries, entries.length)
+    }
+
+    // The state is a map {"rejected": {"timestamp": "2022..."}}
+    // Apple's has a version which we ignore
+    // Instead, map as ["rejected", "2022..."]
+    let [k, v] = entries[0]
+
+    return [k, v["timestamp"]]
+}
+
+function normalizeTemporalInterval(interval) {
+    if (interval === undefined) {
+        return [undefined, undefined]
+    }
+    const start = interval["start_inclusive"]
+    const end = interval["end_exclusive"]
+
+    return [start, end]
+}
+
 function mergeLog(apple, google) {
     let merged = new Map();
 
-    for (const [k, v] of apple) {
-        if (google.has(k)) {
-            if (google.get(k) === v) {
-                // Both the same
-                merged.set(k, v)
+    const operator = apple.get("operator") || google.get("operator")
+
+    const keys = new Set(apple.keys()).union(new Set(google.keys()));
+    for (let k of keys) {
+        let av = apple.get(k)
+        let gv = google.get(k)
+
+        if (k === "description") {
+            av = normalizeDescription(operator, av)
+            gv = normalizeDescription(operator, gv)
+        }
+
+        if (k === "temporal_interval") {
+            let [apple_start, apple_end] = normalizeTemporalInterval(av)
+            let [google_start, google_end] = normalizeTemporalInterval(gv)
+
+            if (apple_start === google_start) {
+                merged.set("start", apple_start)
             } else {
-                merged.set(k + "_apple", v)
-                merged.set(k + "_google", google.get(k))
+                merged.set("apple_start", apple_start)
+                merged.set("google_start", google_start)
             }
+
+            if (apple_end === google_end) {
+                merged.set("end", apple_end)
+            } else {
+                merged.set("apple_end", apple_end)
+                merged.set("google_end", google_end)
+            }
+
+            continue;
+        }
+
+        if (k === "state") {
+            let [apple_status, apple_timestamp] = normalizeState(av)
+            let [google_status, google_timestamp] = normalizeState(gv)
+
+            if (apple_status === google_status) {
+                merged.set("status", apple_status)
+            } else {
+                merged.set("apple_status", apple_status)
+                merged.set("google_status", google_status)
+            }
+
+            if (apple_timestamp === google_timestamp) {
+                merged.set("timestamp", apple_timestamp)
+            } else {
+                merged.set("apple_timestamp", apple_timestamp)
+                merged.set("google_timestamp", google_timestamp)
+            }
+            continue;
+        }
+
+        if (gv === av) {
+            // Both the same
+            merged.set(k, av);
+        } else {
+            merged.set(k + "_apple", av);
+            merged.set(k + "_google", gv);
         }
     }
 
-    for (const [k, v] of google) {
-        if (!apple.has(k)) {
-            merged.set(k, v)
+    return merged;
+}
+
+// for logs in one operator only:
+function normalizeLog(log) {
+    let merged = new Map();
+    for (let [k, v] of log) {
+        if (k === "description") {
+            v = normalizeDescription(log.get("operator"), v);
         }
+
+        if (k === "temporal_interval") {
+            let [start, end] = normalizeTemporalInterval(v);
+            merged.set("start", start);
+            merged.set("end", end);
+
+            continue;
+        }
+
+        if (k === "state") {
+            let [status, timestamp] = normalizeState(v)
+            merged.set("status", status)
+            merged.set("timestamp", timestamp)
+
+            continue;
+        }
+
+        merged.set(k, v);
     }
 
     return merged;
@@ -40,16 +170,23 @@ function dataMerge(apple, google) {
     let googleMap = mapData(google);
 
     let data = new Map();
+    let qualified = new Map();
+    let retired = new Map();
 
     for (const [k, v] of appleMap) {
-        let value = mergeLog(v, googleMap.get(k));
-        data.set(k, value);
+        if (!googleMap.has(k)) {
+            // apple only
+            data.set(k, normalizeLog(v))
+        }
+
+        // Merge if in both:
+        data.set(k, mergeLog(v, googleMap.get(k)));
     }
 
     for (const [k, v] of googleMap) {
         // google only
         if (!appleMap.has(k)) {
-            data.set(k, v);
+            data.set(k, normalizeLog(v));
         }
     }
 
@@ -75,12 +212,13 @@ async function getLogs() {
 async function render() {
     const data = await getLogs();
 
-    const logs = document.querySelector('#logs');
+    const usable = document.querySelector('#usable');
+    const qualified = document.querySelector('#qualified');
+    const other = document.querySelector('#other');
 
     for (const [url, value] of data) {
         let logTitle = document.createElement('h2');
         logTitle.innerHTML = url
-        logs.appendChild(logTitle);
 
         let logTable = document.createElement('table');
 
@@ -88,15 +226,29 @@ async function render() {
             let label = document.createElement('td');
             label.innerText = k;
             let data = document.createElement('td');
-            data.innerHTML = JSON.stringify(v);
+            data.innerHTML = v;
             let row = document.createElement('tr');
             row.appendChild(label);
             row.appendChild(data);
             logTable.appendChild(row);
         }
-        logs.appendChild(logTable);
-    }
 
+        console.log(url)
+        console.log(value)
+        if (value.get("status") === "usable") {
+            usable.appendChild(logTitle);
+            usable.appendChild(logTable);
+        } else if ((value.get("status") === "qualified") ||
+            (value.get("apple_status") === "qualified" && value.get("google_status") === "usable") ||
+            (value.get("apple_status") === "usable" && value.get("google_status") === "qualified")) {
+            qualified.appendChild(logTitle);
+            qualified.appendChild(logTable);
+        } else {
+            other.appendChild(logTitle);
+            other.appendChild(logTable);
+        }
+
+    }
 }
 
 render();
